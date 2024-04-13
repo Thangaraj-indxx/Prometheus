5feabd4ec916df1595e76125dbab772400d33e5f
Thangaraj-indxx <141306308+Thangaraj-indxx@users.noreply.github.com>
2024-04-13 18:43:54 +0530

    Update install-grafana.sh

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,4 +1,5 @@
 sudo apt-get install -y adduser libfontconfig1
+sudo yum install -y adduser libfontconfig1
 wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
 sudo dpkg -i grafana_7.3.4_amd64.deb
 sudo systemctl daemon-reload
